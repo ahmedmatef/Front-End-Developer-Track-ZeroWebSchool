f55d8155480a7a80bb0a935f2707c13c3a534e89
Ahmed M Atef <113757964+ahmedmatef@users.noreply.github.com>
2022-12-28 22:13:10 +0200

    Add files via upload

---
+++ 04-JavaScript/Code/02-Data Types/011 – Variables Introduction.js
@@ -0,0 +1,13 @@
+var user = "Ahmed";
+var age = 23;
+console.log(user); //Ahmed
+console.log(age); //23
+
+/* 
+=>Anthor way to declare two variables in one line
+
+var user = "Ahmed"; age = 23;
+console.log(user); //Ahmed
+console.log(age); //23
+
+*/
